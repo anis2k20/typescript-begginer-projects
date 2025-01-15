514722f80aefa40091019436c71461d615f9ee0a
Anis <anis2k20@gmail.com>
2025-01-15 23:59:22 +0600

    input show in todo list

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -2,12 +2,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("todo-form");
     const input = document.getElementById("todo-input");
+    const listElement = document.getElementById("todo-list");
+    let todos = [];
     if (!form || !input) {
         console.error("No Data Found!");
         return;
     }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        console.log(input.value);
+        // const newTodo: Todo = {
+        //   id: todos.length + 1,
+        //   title: input.value,
+        // };
+        const newTodo = input.value;
+        todos.push(newTodo);
+        updateTodoList();
     });
+    function updateTodoList() {
+        todos.forEach((item) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = item;
+            listElement.appendChild(listItem);
+        });
+    }
 });
--- todo-list/src/main.ts
+++ todo-list/src/main.ts
@@ -1,17 +1,54 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("todo-form") as HTMLElement | null;
   const input = document.getElementById(
     "todo-input"
   ) as HTMLInputElement | null;
+  const listElement = document.getElementById("todo-list") as HTMLUListElement;
+
+  interface Todo {
+    id: Number;
+    title: String | Number;
+  }
+
+  let todos:string[] = [];
 
   if (!form || !input) {
     console.error("No Data Found!");
     return;
   }
 
+
   form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-
-    console.log(input.value);
+    // const newTodo: Todo = {
+    //   id: todos.length + 1,
+    //   title: input.value,
+    // };
+    const newTodo = input.value;
+    todos.push(newTodo);
+    updateTodoList();
   });
+
+  function updateTodoList() {
+    todos.forEach((item)=>{
+      const listItem = document.createElement("li");
+      listItem.textContent = item;
+      listElement.appendChild(listItem)
+    })
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
