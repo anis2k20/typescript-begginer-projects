a39d1cf6e27b5e1e5d1c8a2d299cf22b4a913d4e
Anis <anis2k20@gmail.com>
2025-01-18 22:53:14 +0600

    todo add and delete function added

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -10,18 +10,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        // const newTodo: Todo = {
-        //   id: todos.length + 1,
-        //   title: input.value,
-        // };
-        const newTodo = input.value;
-        todos.push(newTodo);
-        updateTodoList();
+        const newTodo = {
+            id: todos.length + 1,
+            title: input.value,
+        };
+        // const newTodo = input.value;
+        // push unique todo according to its id
+        if (todos.length > 0 && todos.some((item) => item.title === newTodo.title)) {
+            alert("Todo already exists");
+            return;
+        }
+        else {
+            todos.push(newTodo);
+            updateTodoList();
+            input.value = "";
+        }
     });
     function updateTodoList() {
+        listElement.innerHTML = "";
         todos.forEach((item) => {
             const listItem = document.createElement("li");
-            listItem.textContent = item;
+            const action = document.createElement("div");
+            const deleteBtn = document.createElement("button");
+            const editBtn = document.createElement("button");
+            editBtn.textContent = "Edit";
+            deleteBtn.textContent = "Delete";
+            // edit todo
+            editBtn.addEventListener(("click"), () => {
+                const index = todos.findIndex((todo) => todo.id === item.id);
+                const updatedTodo = prompt("Update Todo", item.title.toString());
+                if (updatedTodo) {
+                    todos[index].title = updatedTodo;
+                    updateTodoList();
+                }
+            });
+            // delete todo
+            deleteBtn.addEventListener("click", () => {
+                const index = todos.findIndex((todo) => todo.id === item.id);
+                todos.splice(index, 1);
+                updateTodoList();
+            });
+            listItem.textContent = item.title.toString();
+            action.appendChild(editBtn);
+            action.appendChild(deleteBtn);
+            listItem.appendChild(action);
             listElement.appendChild(listItem);
         });
     }
--- todo-list/src/main.ts
+++ todo-list/src/main.ts
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     title: String | Number;
   }
 
-  let todos:string[] = [];
+  let todos:Todo[] = [];
 
   if (!form || !input) {
     console.error("No Data Found!");
@@ -22,19 +22,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    // const newTodo: Todo = {
-    //   id: todos.length + 1,
-    //   title: input.value,
-    // };
-    const newTodo = input.value;
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      title: input.value,
+    };
+    // const newTodo = input.value;
+
+    // push unique todo according to its id
+
+    if(todos.length > 0 && todos.some((item) => item.title === newTodo.title)) {
+      alert("Todo already exists");
+      return;
+    }else{
     todos.push(newTodo);
     updateTodoList();
+    input.value = "";
+    }
   });
 
   function updateTodoList() {
+    listElement.innerHTML = "";
     todos.forEach((item)=>{
       const listItem = document.createElement("li");
-      listItem.textContent = item;
+      const action = document.createElement("div");
+      const deleteBtn = document.createElement("button");
+      const editBtn = document.createElement("button");
+
+        editBtn.textContent = "Edit";
+        deleteBtn.textContent = "Delete";
+
+        // edit todo
+        editBtn.addEventListener(("click"), ()=>{
+          const index = todos.findIndex((todo) => todo.id === item.id);
+          const updatedTodo = prompt("Update Todo", item.title.toString());
+          if(updatedTodo){
+            todos[index].title = updatedTodo;
+            updateTodoList();
+          }
+        })
+
+        // delete todo
+        deleteBtn.addEventListener("click", () => {
+            const index = todos.findIndex((todo) => todo.id === item.id);
+            todos.splice(index, 1);
+            updateTodoList();
+        });
+
+      listItem.textContent = item.title.toString();
+      action.appendChild(editBtn);
+      action.appendChild(deleteBtn);
+      listItem.appendChild(action);
       listElement.appendChild(listItem)
     })
   }
